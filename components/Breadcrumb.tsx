dc49634739dec8700b70e092ee1e17ae9b279678
Rashid-Hashim-Shaikh <contactrashid8@gmail.com>
2024-09-29 02:44:45 +0530

    :lipstick: Breadcrumb ui enhancement

--- components/Breadcrumb.tsx
+++ components/Breadcrumb.tsx
@@ -12,10 +12,10 @@ const Breadcrumb: React.FC = () => {
   if(!pathArray.length) return null
 
   return (
-    <div aria-label="breadcrumb" className="container mx-auto text-sm text-gray-600">
+    <div aria-label="breadcrumb" className="mx-auto max-w-[1280px] relative  flex w-full  py-6 bg-primary-50/95 container text-sm text-gray-600">
       <ol className="inline-flex space-x-2">
         <li>
-          <Link href="/" className="text-blue-600 hover:underline">
+          <Link href="/" className="text-black hover:underline">
             Home
           </Link>
         </li>
