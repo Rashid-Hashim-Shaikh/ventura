444b9d2247f3003313af6729d5b96ab2806ab220
Rashid-Hashim-Shaikh <contactrashid8@gmail.com>
2024-09-29 05:21:13 +0530

    :sparkles: Add page 404 not found component

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className=" absolute -translate-y-1/2 -translate-x-1/2 top-1/2 left-1/2 ">
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
+};
+
+export default NotFound;
