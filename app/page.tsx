fc615e0b3293afb0b5a35fd2531029eb14f70dd7
Rashid-Hashim-Shaikh <contactrashid8@gmail.com>
2024-09-29 00:40:41 +0530

    :sparkles: Format date range using helper method

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <Link className="grid grid-cols-4 border-b last:border-none" href="/ipo">
+    <div className="mx-auto flex items-center justify-center text-center h-[80vh] w-full ">
+
+    <Link className="border-b border-black" href="/ipo">
       Go to Ipo page
     </Link>
+    </div>
   );
 }
